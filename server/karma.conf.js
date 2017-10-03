19fcc90e474f144b6eb087c4e69ab41d91460339
chfoers <crix93@yahoo.de>
2017-10-03 19:24:20 +0200

    Istanbul integriert, Fehlermeldung bzgl. npm...

--- server/karma.conf.js
+++ server/karma.conf.js
@@ -25,15 +25,18 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
+    preprocessors: { 'src/**/!(*spec).js': ['coverage']
     },
 
-
+    remapIstanbulReporter: {
+reports: { html: 'coverage/html' }
+},
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['progress', 'coverage', 'karma-remap-istanbul'],
+    remapIstanbulReporter: {reports: { html: 'coverage/html' }
+},      
 
     // web server port
     port: 9876,
